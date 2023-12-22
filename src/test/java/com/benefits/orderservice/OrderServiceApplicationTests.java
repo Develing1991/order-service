321e126b462b3e7f47d215133df996d4b17a2052
Develing1991 <completed0728@gmail.com>
2023-12-22 23:47:06 +0900

    init: initialize project

---
+++ src/test/java/com/benefits/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benefits.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
